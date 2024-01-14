b30ae7455cbde1ee8a0c6a613241d15a94d6ed95
Chiranjeevi R <iamchiranjeeviramakrishna@gmail.com>
2024-01-14 17:23:43 +0530

    Added 404 page to the Website

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def home():
 def login():
     return render_template('login.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
 if __name__ == "__main__":
     app.run(debug=True)
