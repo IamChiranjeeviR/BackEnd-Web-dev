5a276eea28475bff34b8b50c3f22641f9e2465e1
Chiranjeevi R <iamchiranjeeviramakrishna@gmail.com>
2024-01-15 16:36:07 +0530

    added Flash messages Feature

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for, flash
+from datetime import datetime
+from logging import DEBUG
 app = Flask(__name__)
+
+app.logger.setLevel(DEBUG)
+app.secret_key = b'x\x12\x1cM\xe5@\x97\xb0'
+fdbk = []
+
+def store_feed(url):
+    fdbk.append(dict(
+        url = url,
+        name = 'Chiru',
+        date = datetime.utcnow()
+    ))
 
 @app.route("/")
 @app.route("/home")
@@ -11,9 +23,16 @@ def home():
 def login():
     return render_template('login.html')
 
-@app.route("/feedBack")
+@app.route("/feedBack", methods=['GET','POST'])
 def feedBack():
-    return render_template('feedBack.html')
+    if request.method == "POST":
+        url = request.form.get('url')
+        store_feed(url)
+        app.logger.debug('Stored Feedback: ' + url)
+        flash("Thanks for Your Feedback :)")
+        return redirect(url_for('home'))
+    
+    return render_template('feedBack.html') 
 
 @app.errorhandler(404)
 def page_not_found(e):
